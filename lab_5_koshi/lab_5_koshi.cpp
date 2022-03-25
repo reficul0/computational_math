3e39547cb5dbe268082e2fe34a4bcd64ebbcabc0
reficul0 <cheshirwr@mail.ru>
2022-03-25 12:07:59 +0700

    Koshi final.

--- lab_5_koshi/lab_5_koshi.cpp
+++ lab_5_koshi/lab_5_koshi.cpp
@@ -10,99 +10,79 @@ void metodRK(
     double x,
     double xLast,
     double y,
+    double z,
     double step,
-    std::function<double(double, double)> func,
-    std::map<double, double>& x_yContainer)
+    std::function<double(double, double, double)> f,
+    std::function<double(double, double, double)> g,
+    std::vector<std::tuple<double, double, double>>& x_y_zContainer)
 {
+    // h = (xLast - x) / n
     int n = static_cast<int>((xLast - x) / step);
-    x_yContainer[x] = y;
+
+    x_y_zContainer.emplace_back(x, y, z);
     for (int i = 0; i < n; i++)
     {
-        bool isPointExist = true;
-        try
-        {
-            double k1 = step * func(x, y);
-            double k2 = step * func(x + step / 2.0, y + k1 / 2.0);
-            double k3 = step * func(x + step / 2.0, y + k2 / 2.0);
-            double k4 = step * func(x + step, y + k3);
+        double k1 = step * f(x, y, z);
+        double l1 = step * g(x, y, z);
 
-            y += (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
-        }
-        catch (const std::runtime_error&)
-        {
-            isPointExist = false;
-        }
+        double k2 = step * f(x + step / 2.0, y + k1 / 2.0, z + l1 / 2.0);
+        double l2 = step * g(x + step / 2.0, y + k1 / 2.0, z + l1 / 2.0);
+
+        double k3 = step * f(x + step / 2.0, y + k2 / 2.0, z + l2 / 2.0);
+        double l3 = step * g(x + step / 2.0, y + k2 / 2.0, z + l2 / 2.0);
+
+        double k4 = step * f(x + step, y + k3, z + l3);
+        double l4 = step * g(x + step, y + k3, z + l3);
+
+        y += (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
+        z += (l1 + 2 * l2 + 2 * l3 + l4) / 6.0;
 
         x += step;
-        if (isPointExist)
-        {
-            x_yContainer[x] = y;
-        }
+        x_y_zContainer.emplace_back(x, y, z);
     }
 }
 
 int main()
 {
-    auto func = [](double x, double z)
+    auto f = [](double x, double y, double z)
     {
-        return (-x)*z;
+        return (z-y)*x;
     };
-	auto func2 = [](double x, double y)
+	auto g = [](double x, double y, double z)
     {
-        if (x == .0)
-            throw std::runtime_error("x == 0 isn't exists");
-
-        return y/x;
+        return (z + y) * x;
     };
 
     double step = .1;
-    double xFirst = 0;
-    double xLast = 1;
-    double zFirst = 1;
-    double yFirst = 0;
 
-    std::map<double, double> x_zContainer;
-    {
-
-	    metodRK(xFirst, xLast, zFirst, step, func, x_zContainer);
-
-	    std::cout << std::setprecision(10);
-	    for (std::pair<const double, double> const& x_y : x_zContainer)
-	    {
-	        std::cout << "x =  " << x_y.first << " : " << ",  y =  " << x_y.second << std::endl;
-	    }
-    }
-
-    std::map<double, double> x_yContainer;
-    {
-        metodRK(xFirst, xLast, yFirst, step, func2, x_yContainer);
-        for (std::pair<const double, double> const& x_y : x_yContainer)
-        {
-            std::cout << "x =  " << x_y.first << " : " << ",  y =  " << x_y.second << std::endl;
-        }
-    }
-
-    std::vector<std::tuple<double, double, double>> x_y_z;
-    for(std::pair<const double, double>& x_z : x_zContainer)
-    {
-        auto x_yIter = x_yContainer.find(x_z.first);
-	    if(x_yIter != x_yContainer.end())
-	    {
-            x_y_z.push_back(std::make_tuple(x_z.first, x_yIter->second, x_z.second));
-	    }
-    }
-
-
-    std::cout << "solution { y' = -x*z; z' = y/x }"
-				<< " x from " << xFirst << " to " << xLast
+    std::cout << "Enter step: ";
+    std::cin >> step;
+    double xFirst = .0;
+    double xLast = 1.;
+    double zFirst = 1.;
+    double yFirst = .0;
+    
+    std::vector<std::tuple<double, double, double>> x_y_zContainer;
+    metodRK(xFirst, xLast, yFirst, zFirst, step, f, g, x_y_zContainer);
+    
+    std::cout << "{\n\ty' = (z-y)*x\n\tz' = (z+y)*x\n}"
+				<< "\n\for x from " << xFirst << " to " << xLast
 				<< " y(0) = " << yFirst
-				<< " z(0) = " << zFirst;
-    for (auto const& x_y_z : x_y_z)
+				<< " z(0) = " << zFirst << std::endl;
+
+    std::cout << "\nSolution:" << std::setprecision(5);
+    std::cout << std::endl
+        << std::setw(8) << std::left << "x"
+        << std::setw(8) << std::left << " y"
+        << std::setw(8) << std::left << "  z" << std::endl;
+    for (auto const& x_y_z : x_y_zContainer)
     {
         double x, y, z;
         std::tie(x, y, z) = x_y_z;
-
-        std::cout << std::endl << "x =  " << x << ",  y =  " << y << ", z =" << z << std::endl;
+        std::cout << std::endl
+    		<< std::setw(8) << std::left << x << " "
+    		<< std::setw(8) << std::left << y << " "
+            << std::setw(8) << std::left << z << std::endl;
     }
 
     std::cout << "========================================================================" << std::endl;
